32a26c5afdf2003c550abce3f81d1ca9ae532e94
jimmy <you@example.com>
2018-10-12 22:35:20 +0800

    update

--- grpcconnect.js
+++ grpcconnect.js
@@ -2,7 +2,7 @@ var grpc = require('grpc');
 var PROTO_PATH = './proto/helloworld.proto';
 
 var proto = grpc.load(PROTO_PATH).helloworld;
-var client = new proto.Greeter('0.0.0.0:59978', grpc.credentials.createInsecure());
+var client = new proto.Greeter('0.0.0.0:50052', grpc.credentials.createInsecure());
 
 function SayHello(params) {
     return new Promise(function(resolve, reject) {
